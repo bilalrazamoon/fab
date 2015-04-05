39a896650f2dc560c4e78fa9f985ac61af45095f
hafizbilal112 <hafizbilal112@gmail.com>
2015-04-06 00:04:38 +0500

    Added fabscrollcontainer directive and added to disabled livedisplace option.

--- fab.js
+++ fab.js
@@ -1,9 +1,18 @@
 angular.module('fab-component', [])
+    .directive('fabScrollContainer', function () {
+        return {
+            restrict: 'A',
+            controller: ['$element', function ($element) {
+                $element.parent().data('fabScrollContainer', $element);
+            }]
+        };
+    })
     .directive('fab', function() {
         return {
             restrict: 'E',
             replace: true,
             transclude: true,
+            scope: {'liveDisplace':'='},
             template: template,
             link: link
         };
@@ -19,16 +28,28 @@ angular.module('fab-component', [])
         }
         //link
         function link(scope, element, attr) {
-            var targetId = attr['targetId'];
-            var targetEl = angular.element(document.getElementById(targetId));
+            var liveDisplace = scope.liveDisplace===false?scope.liveDisplace:true;
+            var targetEl = element.inheritedData('fabScrollContainer') || window;
             element.addClass('animated');
             var scroll = 0, max = 80, current = 0, prevScroll = 0;
             targetEl.bind('scroll', function (e) {
                 scroll = window.ionic?e.detail.scrollTop:e.target.scrollTop;
-                current = scroll >= 0 ? Math.min(max, Math.max(0, current + scroll - prevScroll)) : 0;
-                window.requestAnimationFrame(function () {
-                    element.css("transform","translate3d(0, " + current + "px, 0)");
-                });
+                if(liveDisplace){
+                    current = scroll >= 0 ? Math.min(max, Math.max(0, current + scroll - prevScroll)) : 0;
+                    window.requestAnimationFrame(function () {
+                        element.css("transform","translate3d(0, " + current + "px, 0)");
+                    });
+                }
+                else{
+                    if (current < scroll) {
+                        current = scroll;
+                        element.removeClass('fadeInUp').addClass('fadeOutDown');
+                    }
+                    if (current > scroll) {
+                        current = scroll;
+                        element.removeClass('fadeOutDown').addClass('fadeInUp');
+                    }
+                }
                 prevScroll = scroll;
             });
         }
